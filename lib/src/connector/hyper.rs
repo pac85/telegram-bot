a507c5e1b3da3b4196631c8a9d9585aee37752e0
antonino <antomani103@gmail.com>
2018-11-03 00:01:40 +0100

    modified:   lib/src/connector/hyper.rs

--- lib/src/connector/hyper.rs
+++ lib/src/connector/hyper.rs
@@ -83,9 +83,10 @@ impl<C: Connect> Connector for HyperConnector<C> {
 
 /// Returns default hyper connector. Uses one resolve thread and `HttpsConnector`.
 pub fn default_connector(handle: &Handle) -> Result<Box<Connector>, Error> {
-    let connector = HttpsConnector::new(1).map_err(|err| {
+    /*let connector = HttpsConnector::new(1, handle).map_err(|err| {
         ::std::io::Error::new(::std::io::ErrorKind::Other, format!("tls error: {}", err))
     })?;
     let config = Client::configure().connector(connector);
+    Ok(Box::new(HyperConnector::new(config.build(handle))))*/
     Ok(Box::new(HyperConnector::new(1)))
 }
